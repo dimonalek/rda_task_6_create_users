d29268d5b485734155a9f218e4b8f0f4382a2ad9
dimonalek <dimonalek@gmail.com>
2026-04-21 11:53:25 +0300

    Minor fixes

--- task.sql
+++ task.sql
@@ -1,4 +1,4 @@
-create user IF NOT EXISTS 'webappuser'@'localhost' identified by 'P@ssw0rd';
-create user IF NOT EXISTS 'deploymentuser'@'localhost' identified by 'P@ssw0rd';
-grant all on ShopDB.* to 'deploymentuser'@'localhost';
-grant select, insert, update, delete on ShopDB.* to 'webappuser'@'localhost';
+create user IF NOT EXISTS 'webappuser'@'%' identified by 'P@ssw0rd';
+create user IF NOT EXISTS 'deploymentuser'@'%' identified by 'P@ssw0rd';
+grant all on ShopDB.* to 'deploymentuser'@'%';
+grant select, insert, update, delete on ShopDB.* to 'webappuser'@'%';
