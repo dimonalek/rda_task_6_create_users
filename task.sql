09c67916188a71709afabb77b795923b7abb4282
dimonalek <dimonalek@gmail.com>
2026-04-21 11:46:37 +0300

    Created Users

--- task.sql
+++ task.sql
@@ -1,5 +1,4 @@
--- write your code for database user creation here
-create user 'webappuser'@'%' identified by 'P@ssw0rd';
-create user 'deploymentuser'@'%' identified by 'P@ssw0rd';
-grant all on ShopDB.* to 'deploymentuser'@'%';
-grant select, insert, update, delete on ShopDB.* to 'webappuser'@'%';
+create user 'webappuser'@'localhost' identified by 'P@ssw0rd';
+create user 'deploymentuser'@'localhost' identified by 'P@ssw0rd';
+grant all on ShopDB.* to 'deploymentuser'@'localhost';
+grant select, insert, update, delete on ShopDB.* to 'webappuser'@'localhost';
